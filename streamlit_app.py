d554b5f6007381e8427622abbd674d5756515605
FilippoPietroni <fpietroni@bitbang.com>
2023-09-20 15:02:55 +0200

    refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,9 +51,17 @@ if streamlit.button('Get fruit data list'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-fruit_choice_add = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write("Thank you for entering", fruit_choice_add)
-my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values ('{fruit_choice_add}')")
+
+def get_insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values ('{fruit_choice_add}')")
+        return "Thanks for adding " + new_fruit
+
+
+fruit_choice_add = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button("Add new fruit"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = get_insert_row_snowflake(fruit_choice_add)
+    streamlit.write(back_from_function)
 
 
